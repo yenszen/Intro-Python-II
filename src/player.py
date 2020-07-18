11fe4c4bfd3d3e8a0e45c0ec66a11e5300a994a2
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-07-19 00:34:11 +0800

    completed mvp

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.current_room = None
+        self.inventory = []
+
+    def add_item(self, item):
+        self.inventory.append(item)
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
